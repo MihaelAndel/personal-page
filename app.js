6c0c3132aa7fe54bcf2d85997a52283e73245324
Mihael Anđel <andel.mihael@gmail.com>
2019-10-11 16:49:55 +0200

    Slike i animacije

---
+++ app.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+	let cover = document.querySelector('#cover');
+	let loading = document.querySelector('#loading');
+	cover.style.display = 'none';
+	loading.style.display = 'none';
+};
