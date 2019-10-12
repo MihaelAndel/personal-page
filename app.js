e97af156b42d03c19bb661b65f7fa08787e8494c
Mihael Anđel <andel.mihael@gmail.com>
2019-10-12 14:12:23 +0200

    loading fix animacije

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 function hide() {
 	let cover = document.querySelector('#cover');
 	let loading = document.querySelector('#loading');
+	let fadingElements = document.querySelectorAll('.fade');
+	let title = document.querySelector('#title');
+
 	cover.style.display = 'none';
 	loading.style.display = 'none';
+
+	title.style.animationName = 'title-animation';
+
+	fadingElements.forEach(element => {
+		element.style.animationName = 'fade';
+	});
 }
 
 window.addEventListener('load', () => {
-	//alert('load');
 	hide();
 });
 
